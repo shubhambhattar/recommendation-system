2d8199f7537faab72c32dca046d8811fe290d20c
Shubham Bhattar <shubhambhattar@users.noreply.github.com>
2016-11-28 23:33:42 +0530

    Some major changes. See description.
    - The first code had a problem. I had generated the id of the movies myself, instead of using the supplied ones. It turns out that the supplied one can be useful because the file links.csv can then be used to get a movie's corresponding IMDB or TheMovieDB id.
    
    - Simplified the structure of code here and there, mainly the Movie_Genres table part.

--- extract.py
+++ extract.py
@@ -1,6 +1,11 @@
 # Extract data from the csv files and organise them as tables in a databse.
 
 import csv
+import sqlite3
+
+###############################################################################
+###############              For data/movies.csv             ##################
+###############################################################################
 
 # Values from "data/movies.csv" file stored as list of tuples in data
 data = []
@@ -9,21 +14,21 @@ with open('data/movies.csv', 'rb') as csvfile:
     for row in datareader:
         data.append(tuple(row))
 
-"""
-for item in data[:60]:
-    print item
-"""
+# delete the row that contains metadata (id, title, genre).
+del data[0]
 
 # movie_table stores the list of all the movies from "data/movies.csv"
+# it stores tuples of the form (movie_id, movie_name).
 movie_table = []
-for item in data[1:]:
-    movie_table.append(item[1])
+for item in data:
+    movie_table.append((item[0], item[1], ))
 
-"""
+# this dictionary is used to find the corresponding id of the movie when the
+# title of the movie is known.
+movie_table_dict = {}
 for item in movie_table:
-    print item
-print len(movie_table)
-"""
+    movie_table_dict[item[1]] = item[0]
+
 
 # genre_table_temp used to extract all the unique genres from "data/movies.csv"
 genre_table_temp = set()
@@ -32,40 +37,24 @@ for item in data:
     for value in values:
         genre_table_temp.add(value)
 
-# genre_table stores the final list of all genres as a list of values
-genre_table = []
-for item in genre_table_temp:
-    genre_table.append(item)
 
-"""
-for item in genre_table:
-    print item
-"""
+# genre_table stores the final list of all genres as a list of values
+genre_table = list(genre_table_temp)
+
 
 # movie_genre is a many-many relationship between movies and genre. Because
 # each movie can have multiple genre and each genre can have multiple movies.
-movie_genre = []
-movie_genre_temp = []
-for item in data[1:]:
-    movie_genre_temp.append((item[1], item[2].split('|'), ))
 
-for item in movie_genre_temp:
-    genre_list = []
-    for value in item[1]:
-        genre_list.append(genre_table.index(value)+1)
-    movie_index = movie_table.index(item[0])+1
+movie_genre = []
+for item in data:
+    # genre_list contains the index of all the genres of a particular movie.
+    genre_list = [genre_table.index(value)+1 for value in item[2].split('|')]
+    movie_index = movie_table_dict[item[1]]
     for value in genre_list:
         movie_genre.append((movie_index, value, ))
 
 
-"""
-for item in movie_genre:
-    print item
-"""
-
 #### Creating the database
-
-import sqlite3
 
 # establish a connection between the sqlite database
 conn = sqlite3.connect('moviesdb.sqlite')
@@ -81,7 +70,7 @@ cur.executescript('''
     DROP TABLE IF EXISTS Genres;
     DROP TABLE IF EXISTS Movies_Genres;
     CREATE TABLE Movies (
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        id INTEGER NOT NULL PRIMARY KEY,
         name TEXT
     );
     CREATE TABLE Genres (
@@ -98,7 +87,7 @@ cur.executescript('''
 # Insert data in the Movies (id, name) table
 for movie in movie_table:
     cur.execute('''
-        INSERT INTO Movies (name) VALUES (?)''', (movie, ))
+        INSERT INTO Movies VALUES (?, ?)''', (movie[0], movie[1], ))
 
 # save the changes made in the databse
 conn.commit()
